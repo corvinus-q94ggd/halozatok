f8daf26b190e781f1d5a4e249f0f741eac4f6d4d
Attila Matyi <attila.matyi@stud.uni-corvinus.hu>
2021-04-25 20:34:14 +0200

    rest api

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -2,31 +2,61 @@
 var kérdésszám;
 
 function letöltés() {
-    fetch('/questions.json')
+    //fetch('/questions.json')
+    //    .then(response => response.json())
+    //    .then(data => letöltésBefejeződött(data));
+
+    fetch('/questions/1')
         .then(response => response.json())
-        .then(data => letöltésBefejeződött(data));
+        .then(data => kérdésMegjelenítés(data)
+        );
 }
 
-function letöltésBefejeződött(d) {
-    console.log("Sikeres letöltés")
-    console.log(d)
-    kérdések = d;
-    kérdésMegjelenítés(0);
+//function letöltésBefejeződött(d) {
+//    console.log("Sikeres letöltés")
+//    console.log(d)
+//    kérdések = d;
+//    kérdésMegjelenítés(0);
+//}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+    fetch(`/questions/${id}`)
+        .then(válaszfeldolgozás)
+        .then(kérdésMegjelenítés);
+}   
+
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
+    }
 }
 
-var kérdésMegjelenítés = function (kérdésSzáma) {
-    let kérdés_szöveg = document.getElementById("kérdés_szöveg");
-    let válasz1 = document.getElementById("válasz1");
-    let válasz2 = document.getElementById("válasz2");
-    let válasz3 = document.getElementById("válasz3");
-    let kép = document.getElementById("kép1");
-
-    kérdés_szöveg.innerHTML = kérdések[kérdésSzáma].questionText
-    kép.src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdésSzáma].image
-    válasz1.innerText = kérdések[kérdésSzáma].answer1
-    válasz2.innerText = kérdések[kérdésSzáma].answer2
-    válasz3.innerText = kérdések[kérdésSzáma].answer3
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
 }
+
+//var kérdésMegjelenítés = function (kérdésSzáma) {
+//    let kérdés_szöveg = document.getElementById("kérdés_szöveg");
+//    let válasz1 = document.getElementById("válasz1");
+//    let válasz2 = document.getElementById("válasz2");
+//    let válasz3 = document.getElementById("válasz3");
+//    let kép = document.getElementById("kép1");
+
+//    kérdés_szöveg.innerHTML = kérdések[kérdésSzáma].questionText
+//    kép.src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdésSzáma].image
+//    válasz1.innerText = kérdések[kérdésSzáma].answer1
+//    válasz2.innerText = kérdések[kérdésSzáma].answer2
+//    válasz3.innerText = kérdések[kérdésSzáma].answer3
+//}
 
 window.onload = () => {
     letöltés();
